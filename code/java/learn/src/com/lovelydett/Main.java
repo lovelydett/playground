a79d76ae5d97e60f055a61f02310c94477fd2295
XieYuting <441875808@qq.com>
2020-09-26 14:09:55 +0800

    java学习项目

---
+++ code/java/learn/src/com/lovelydett/Main.java
@@ -0,0 +1,10 @@
+package com.lovelydett;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello, my pink");
+        System.out.println("this is my java playground");
+    }
+}
