c6418817af885c8de4f7cbd7c4186f01bf3f3d49
Yuting.Xie <yuting.xie@weride.ai>
2021-05-27 16:50:29 +0800

    C++ member function ptr

--- code/C++/memFunc.cpp
+++ code/C++/memFunc.cpp
@@ -1,5 +1,5 @@
-#include<cstdio>
-
+#include <cstdio>
+#include <functional>
 
 class A{
     public:
@@ -8,6 +8,9 @@ class A{
         }
         void func2(){
             printf("func2 of A called\n");
+        }
+        void func3(){
+            printf("func3 of A called\n");
         }
         static void sfunc(){
             printf("static func of A called\n");
@@ -20,5 +23,7 @@ int main(){
     A a2;
     A a3;
 
-    printf("a.func1:%p, a.func2:%p, a.sfunc:%p\n", &A::func1, );
+    printf("a.func1:%p, a.func2:%p, a.func3:%p, a.sfunc:%p\n", &A::func1, &A::func2, &A::func3, &A::sfunc);
+    printf("std::bind(&A::func2, &a2) : %p\n", std::bind(&A::func2, &a2));
+    return 0;
 }
